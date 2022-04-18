1e1085c4c61171a5ae9bf715691426339abbfe4c
Tyrell C <tyrellrcurry@gmail.com>
2022-04-18 01:48:27 -0700

    Tailwind & Axios init. Tailwind custom colours added. Outfit Font added as default.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <h1>Entertainment App Home Page</h1>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      // sunset orange
+      'x-orange': '#FC4747',
+
+      // blue-ish gray
+      'x-gray': '#5A698F',
+
+      // dark black
+      'x-vulcan': '#10141E',
+
+      // dark blue
+      'x-mirage': '#161D2F',
+
+      // white
+      'x-white': '#FFFFFF',
+    },
+  },
+  plugins: [],
+};
