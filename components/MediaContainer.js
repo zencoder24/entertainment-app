43c81349f1854367201dfa25d64f57d2673faad2
Tyrell C <tyrellrcurry@gmail.com>
2022-04-19 23:43:00 -0700

    Added bottom padding to MediaContainer

--- components/MediaContainer.js
+++ components/MediaContainer.js
@@ -6,7 +6,7 @@ export default function MediaContainer({children, title}) {
       <div className="title px-4 py-6 text-2xl font-thin md:py-8 lg:py-10 xl:py-12 md:text-3xl lg:text-5xl">
         {title}
       </div>
-      <div className="grid grid-cols-2 gap-4 px-4 md:grid-cols-3">
+      <div className="grid grid-cols-2 gap-4 px-4 pb-6 md:grid-cols-3 md:pb-12">
         {children}
       </div>
     </div>
