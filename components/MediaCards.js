7651b5dade64a8a38a44d5687dbe419b05366e80
Tyrell C <tyrellrcurry@gmail.com>
2022-04-21 00:09:26 -0700

    Comments added

--- components/MediaCards.js
+++ components/MediaCards.js
@@ -10,6 +10,8 @@ export default function MediaCards({
   title,
 }) {
   const [isBookmarked, setIsBookmarked] = useState(false);
+
+  //Hover states, these will be passed as class variables or as booleans (to display elements):
   const [bookmarkHover, setBookmarkHover] = useState();
   const [bookmarkIconHover, setBookmarkIconHover] = useState();
   const [playHover, setPlayHover] = useState(false);
@@ -20,8 +22,10 @@ export default function MediaCards({
       <div className="img">
         <div
           className="bookmark relative flex justify-end cursor-pointer"
+          //Setting mouse events here because this is the card container, this is to darken the img and add a play button
           onMouseEnter={() => {
             setPlayHover(true);
+            //'hover-img' is declared in global
             setImgHover('hover-img');
           }}
           onMouseLeave={() => {
@@ -29,7 +33,7 @@ export default function MediaCards({
             setImgHover('');
           }}
         >
-          {/* If playHover is true, display these elements */}
+          {/* If playHover is true, display these elements, boolean set in element above */}
           {playHover && (
             <div className="hidden md:flex div play-icon absolute bg-x-white bg-opacity-25 px-4 py-2 rounded-full justify-center items-center z-10">
               <img src="/assets/icon-play.svg" alt="" className="w-7 mr-2" />
@@ -40,13 +44,15 @@ export default function MediaCards({
           <div
             className={`bg-x-mirage z-10 absolute w-6 h-6 flex justify-center items-center rounded-full mt-1 mr-1 opacity-70 md:w-10 md:h-10 md:mt-3 md:mr-3 ${bookmarkHover}`}
             onClick={(e) => setIsBookmarked(!isBookmarked)}
-            // On mouse enter, if screen is bigger than md, add these classes
+            // On mouse enter, if screen is bigger than md, add these classes to the BookmarkIconHover state
             onMouseEnter={() => {
               window.innerWidth > 768
-                ? setBookmarkHover('bg-x-white-bk opacity-100 z-2')
+                ? // Bookmark BG
+                  setBookmarkHover('bg-x-white-bk opacity-100 z-2')
                 : '';
               window.innerWidth > 768
-                ? setBookmarkIconHover('hover-bookmark z-2')
+                ? // Bookmark Icon
+                  setBookmarkIconHover('hover-bookmark z-2')
                 : '';
             }}
             //remove classes on mouse leave
