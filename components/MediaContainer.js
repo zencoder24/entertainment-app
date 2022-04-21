5b0bd6a84612202b64f666c6305a25005ac6b602
Rod Garland <rg6797@outlook.com>
2022-04-21 02:40:47 -0400

    Fix the MediaContainer grid columns to go to 4 on lg screen sizes

--- components/MediaContainer.js
+++ components/MediaContainer.js
@@ -6,7 +6,7 @@ export default function MediaContainer({children, title, searchVal}) {
       <div className="title px-4 py-6 text-2xl font-thin md:py-8 lg:py-10 xl:py-12 md:text-3xl lg:text-5xl">
         {title}
       </div>
-      <div className="grid grid-cols-2 gap-4 px-4 pb-6 md:grid-cols-3 md:pb-12">
+      <div className="grid grid-cols-2 gap-4 px-4 pb-6 md:grid-cols-3 md:pb-12 lg:grid-cols-4">
         {children}
       </div>
     </div>
