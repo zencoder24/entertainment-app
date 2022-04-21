1015b19ccb33640b6e5d2c52936f970fbfe8e8ef
Rod Garland <rg6797@outlook.com>
2022-04-21 01:21:12 -0400

    Finish bookmark page style

--- pages/bookmarks/index.jsx
+++ pages/bookmarks/index.jsx
@@ -1,13 +1,61 @@
-import React from 'react';
+import React  from 'react';
+import { useState } from 'react';
 import SearchBar from '../../components/SearchBar';
+import mediaItems from '../../public/data.json';
+import MediaContainer from '../../components/MediaContainer';
+import MediaCards from '../../components/MediaCards'
 
-const BookmarksPage = () => {
+
+export const getStaticProps = async () => {
+    return {
+      props: {
+        mediastuff: mediaItems,
+      },
+    };
+  };
+
+  export default function BookmarksPage ({mediastuff}) {
+  
+    const[searchVal, setSearchVal] = useState("")
+  
+  
     return (
-        <div>
-            <SearchBar placholderText={'Search for bookmarked shows'}/>
-            <h1>Bookmarks Page</h1>
-        </div>
-    );
-};
+      <>
+  
+        <SearchBar searchVal={searchVal} setSearchVal={setSearchVal} placholderText={'Search bookmarked shows'}/>
+  
+        
+          <MediaContainer searchVal={searchVal} title={'Bookmarked Movies'}>
+            {mediastuff
+              .filter((media) => media.category === "Movie" && media.isBookmarked === true)
+              .map((media) => (
+                <MediaCards
+                  small={media.thumbnail.regular.small}
+                  medium={media.thumbnail.regular.medium}
+                  large={media.thumbnail.regular.large}
+                  year={media.year}
+                  category={media.category}
+                  rating={media.rating}
+                  title={media.title}
+                />
+              ))}
+          </MediaContainer>
 
-export default BookmarksPage;
+          <MediaContainer searchVal={searchVal} title={'Bookmarked TV Series'}>
+            {mediastuff
+              .filter((media) => media.category === "TV Series" && media.isBookmarked === true)
+              .map((media) => (
+                <MediaCards
+                  small={media.thumbnail.regular.small}
+                  medium={media.thumbnail.regular.medium}
+                  large={media.thumbnail.regular.large}
+                  year={media.year}
+                  category={media.category}
+                  rating={media.rating}
+                  title={media.title}
+                />
+              ))}
+          </MediaContainer>
+      </>
+    );
+  }
