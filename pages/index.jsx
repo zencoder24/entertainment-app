84649f1f8505a5c06c00340f5a0e31712c25e633
Tyrell C <tyrellrcurry@gmail.com>
2022-04-20 21:48:14 -0700

    Creating movies page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Navbar from '../components/Navbar';
 import SearchBar from '../components/SearchBar';
-
 import mediaItems from '../public/data.json';
-
 import MediaCards from '../components/MediaCards';
 import MediaContainer from '../components/MediaContainer';
-
 import TrendingCards from '../components/TrendingCards';
 import TrendingContainer from '../components/TrendingContainer';
-import { useState } from 'react';
-
-
+import {useState} from 'react';
 
 /**TODO: Need to add keys to the the json file. */
 
@@ -25,47 +17,48 @@ export const getStaticProps = async () => {
 };
 
 export default function Home({mediastuff}) {
-  
-  const[searchVal, setSearchVal] = useState("")
-
+  const [searchVal, setSearchVal] = useState('');
 
   return (
     <>
+      <SearchBar
+        searchVal={searchVal}
+        setSearchVal={setSearchVal}
+        placholderText={'Search for movies or TV series'}
+      />
 
-      <SearchBar searchVal={searchVal} setSearchVal={setSearchVal} placholderText={'Search for movies or TV series'}/>
-
-        {/** Trending Component */}
-        <TrendingContainer searchVal={searchVal}  title={'Trending'}>
-          {mediastuff
+      {/** Trending Component */}
+      <TrendingContainer searchVal={searchVal} title={'Trending'}>
+        {mediastuff
           .filter((media) => media.isTrending === true)
           .map((media) => (
             <TrendingCards
-            year={media.year}
-            category={media.category}
-            rating={media.rating}
-            title={media.title}
-            small={media.thumbnail.trending.small}
-            large={media.thumbnail.trending.large}
+              year={media.year}
+              category={media.category}
+              rating={media.rating}
+              title={media.title}
+              small={media.thumbnail.trending.small}
+              large={media.thumbnail.trending.large}
             />
           ))}
-       </TrendingContainer>
+      </TrendingContainer>
 
-        {/** Trending Component */}
-        <MediaContainer searchVal={searchVal} title={'Recommended for you'}>
-          {mediastuff
-            .filter((media) => media.isTrending === false)
-            .map((media) => (
-              <MediaCards
-                small={media.thumbnail.regular.small}
-                medium={media.thumbnail.regular.medium}
-                large={media.thumbnail.regular.large}
-                year={media.year}
-                category={media.category}
-                rating={media.rating}
-                title={media.title}
-              />
-            ))}
-        </MediaContainer>
+      {/** Trending Component */}
+      <MediaContainer searchVal={searchVal} title={'Recommended for you'}>
+        {mediastuff
+          .filter((media) => media.isTrending === false)
+          .map((media) => (
+            <MediaCards
+              small={media.thumbnail.regular.small}
+              medium={media.thumbnail.regular.medium}
+              large={media.thumbnail.regular.large}
+              year={media.year}
+              category={media.category}
+              rating={media.rating}
+              title={media.title}
+            />
+          ))}
+      </MediaContainer>
     </>
   );
 }
--- pages/movies/index.jsx
+++ pages/movies/index.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
 import SearchBar from '../../components/SearchBar';
+import {useState} from 'react';
+import MediaContainer from '../../components/MediaContainer';
+import MediaCards from '../../components/MediaCards';
+import mediaItems from '../public/data.json';
 
-const MoviesPage = () => {
-    return (
-        <div>
-            <SearchBar placholderText={'Search for movies'}/>
-            <h1>Movies Page</h1>
-        </div>
-    );
+export const getStaticProps = async () => {
+  return {
+    props: {
+      mediastuff: mediaItems,
+    },
+  };
+};
+
+const MoviesPage = ({mediastuff}) => {
+  const [searchVal, setSearchVal] = useState('');
+  return (
+    <div>
+      <SearchBar
+        searchVal={searchVal}
+        setSearchVal={setSearchVal}
+        placholderText={'Search for movies'}
+      />
+
+      <MediaContainer searchVal={searchVal} title={'Movies'}>
+        {mediastuff
+          .filter((media) => media.category === 'Movie')
+          .map((media) => (
+            <MediaCards
+              small={media.thumbnail.regular.small}
+              medium={media.thumbnail.regular.medium}
+              large={media.thumbnail.regular.large}
+              year={media.year}
+              category={media.category}
+              rating={media.rating}
+              title={media.title}
+            />
+          ))}
+      </MediaContainer>
+    </div>
+  );
 };
 
 export default MoviesPage;
