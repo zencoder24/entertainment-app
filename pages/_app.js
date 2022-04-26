78eb6e3efbd8a1a521f3f9c2d8d9eccf2f1d4128
Rod Garland <rg6797@outlook.com>
2022-04-25 22:22:57 -0400

    Added framer styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
-
+import {motion} from 'framer-motion'
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
-function MyApp({Component, pageProps}) {
+function MyApp({Component, pageProps, router}) {
+  const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+}
+
  
   return(
       <Layout>
-        <Component {...pageProps} />
+        <motion.div key={router.route} initial="hidden" animate="enter" exit="exit" transition={{type:'linear'}} variants={variants}>
+          <Component {...pageProps} />
+        </motion.div>
       </Layout>
   ) 
 }
